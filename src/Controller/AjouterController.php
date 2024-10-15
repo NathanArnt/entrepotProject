b90ee57f7099bc6dfb361f0f0e3c055b9129e319
Nathan <narnault.ledantec@gmail.com>
2024-10-15 15:03:47 +0200

    ajouter controller

---
+++ src/Controller/AjouterController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Colis;
+use App\Entity\Distance;
+use App\Entity\Entrepot;
+use App\Entity\Ville;
+use App\Form\ColisFormType;
+use App\Form\DistanceFormType;
+use App\Form\EntrepotFormType;
+use App\Form\VilleFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AjouterController extends AbstractController
+{
+    #[Route('/ajouter', name: 'app_ajouter')]
+    public function index(): Response
+    {
+        return $this->render('ajouter/index.html.twig', [
+            'controller_name' => 'AjouterController',
+        ]);
+    }
+
+    #[Route('/ajouter/colis', name: 'app_ajouter_colis')]
+    public function ajoutercolis(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Création d'une nouvelle instance de l'entité Produit
+        $colis = new Colis();
+    
+        // Création du formulaire en associant l'entité Produit
+        $form = $this->createForm(ColisFormType::class, $colis);
+
+        // Traitement de la requête HTTP
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire a été soumis et si les données sont valides
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // Sauvegarde du produit
+                $colis = $form->getData();
+                $entityManager->persist($colis);
+                $entityManager->flush();
+    
+                // Message de succès
+                $this->addFlash('success', 'Le colis a été ajouté avec succès.');
+    
+                // Redirection
+                return $this->redirectToRoute('app_ajouter_colis');
+            } else {
+                // Message flash si le formulaire est soumis mais invalide
+                $this->addFlash('error', 'Le formulaire contient des erreurs. Veuillez les corriger.');
+            }
+        }
+    
+        // Affichage du formulaire dans la vue Twig
+        return $this->render('ajouter/ajoutercolis.html.twig', [
+            'colisForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/ajouter/distance', name: 'app_ajouter_distance')]
+    public function ajouterDistance(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Création d'une nouvelle instance de l'entité Produit
+        $distance = new Distance();
+    
+        // Création du formulaire en associant l'entité Produit
+        $form = $this->createForm(DistanceFormType::class, $distance);
+
+        // Traitement de la requête HTTP
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire a été soumis et si les données sont valides
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // Sauvegarde du produit
+                $distance = $form->getData();
+                $entityManager->persist($distance);
+                $entityManager->flush();
+    
+                // Message de succès
+                $this->addFlash('success', 'La distance a été ajouté avec succès.');
+    
+                // Redirection
+                return $this->redirectToRoute('app_ajouter_distance');
+            } else {
+                // Message flash si le formulaire est soumis mais invalide
+                $this->addFlash('error', 'Le formulaire contient des erreurs. Veuillez les corriger.');
+            }
+        }
+    
+
+        // Affichage du formulaire dans la vue Twig
+        return $this->render('ajouter/ajouterdistance.html.twig', [
+            'distanceForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/ajouter/entrepot', name: 'app_ajouter_entrepot')]
+    public function ajouterEntrepot(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Création d'une nouvelle instance de l'entité Produit
+        $entrepot = new Entrepot();
+    
+        // Création du formulaire en associant l'entité Produit
+        $form = $this->createForm(EntrepotFormType::class, $entrepot);
+
+        // Traitement de la requête HTTP
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire a été soumis et si les données sont valides
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // Sauvegarde du produit
+                $entrepot = $form->getData();
+                $entityManager->persist($entrepot);
+                $entityManager->flush();
+    
+                // Message de succès
+                $this->addFlash('success', "L'entrepot a été ajouté avec succès.");
+    
+                // Redirection
+                return $this->redirectToRoute('app_ajouter_entrepot');
+            } else {
+                // Message flash si le formulaire est soumis mais invalide
+                $this->addFlash('error', 'Le formulaire contient des erreurs. Veuillez les corriger.');
+            }
+        }
+    
+
+        // Affichage du formulaire dans la vue Twig
+        return $this->render('ajouter/ajouterentrepot.html.twig', [
+            'entrepotForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/ajouter/ville', name: 'app_ajouter_ville')]
+    public function definirDistance(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Création d'une nouvelle instance de l'entité Produit
+        $ville = new Ville();
+    
+        // Création du formulaire en associant l'entité Produit
+        $form = $this->createForm(VilleFormType::class, $ville);
+
+        // Traitement de la requête HTTP
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire a été soumis et si les données sont valides
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // Sauvegarde du produit
+                $ville = $form->getData();
+                $entityManager->persist($ville);
+                $entityManager->flush();
+    
+                // Message de succès
+                $this->addFlash('success', 'La ville a été ajouté avec succès.');
+    
+                // Redirection
+                return $this->redirectToRoute('app_ajouter_ville');
+            } else {
+                // Message flash si le formulaire est soumis mais invalide
+                $this->addFlash('error', 'Le formulaire contient des erreurs. Veuillez les corriger.');
+            }
+        }
+    
+
+        // Affichage du formulaire dans la vue Twig
+        return $this->render('ajouter/ajouterville.html.twig', [
+            'villeForm' => $form->createView(),
+        ]);
+    }
+}
